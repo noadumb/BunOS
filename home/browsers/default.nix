72906c899c0afa62390472c32d157f6ea55d5fec
noelle <noa.magic2006@gmail.com>
2026-08-22 02:43:01 +0200

    make librewolf save cookies

--- home/browsers/default.nix
+++ home/browsers/default.nix
@@ -2,12 +2,14 @@
 {
   programs.librewolf = {
     enable = true;
+
     settings = lib.mkDefault {
       "webgl.disabled" = false;
       "identity.fxaccounts.enabled" = true;
       "privacy.clearOnShutdown.history" = false;
       "privacy.clearOnShutdown.downloads" = false;
       "privacy.clearOnShutdown.cookies" = false;
+      "network.cookie.lifetimePolicy" = 0;
     };
   };
 }
