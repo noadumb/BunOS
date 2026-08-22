84db2e53e1767e3fb7bee584517c97b803329471
noelle <noa.magic2006@gmail.com>
2026-08-22 05:23:40 +0200

    uhhhh clion maybe works idfk

--- home/dev/jetbrains.nix
+++ home/dev/jetbrains.nix
@@ -4,7 +4,25 @@
 }:
 {
   home.packages = with pkgs; [
-    jetbrains-toolbox
     jetbrains.clion
+    gcc
+    gdb
+    cmake
+    gnumake
+    ninja
+    clang-tools
+    nix-ld
   ];
+/*  options = with lib; {
+    bunos.desktop.dev = {
+      jetbrains = {
+        enable = mkEnableOption "jetbrains programs";
+      };
+    };
+  };
+  config = lib.mkIf config.bunos.dev.jetbrains.enable {
+    jetbrains = {
+      enable = true;
+    }; TODO: fix pls
+  }; */
 }
