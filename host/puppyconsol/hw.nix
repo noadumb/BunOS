028bb19635ccd96e6611a58604f05fd2730a281a
noelle <noa.magic2006@gmail.com>
2026-08-30 16:35:43 +0200

    added puppyconsol for legion go :3

---
+++ host/puppyconsol/hw.nix
@@ -0,0 +1,139 @@
+{
+  config,
+  lib,
+  modulesPath,
+  ...
+}:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "xhci_pci"
+    "thunderbolt"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+    "rtsx_pci_sdmmc"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+/*  fileSystems."/" = { #TODO:remove these and replace with disko
+    device = "/dev/disk/by-uuid/e626a254-bb8e-45ca-a55c-df2c77a5ddc1";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/56A7-3E48";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
+    ];
+  }; */
+
+  disko.devices = {
+    disk = {
+      nvme = {
+        device = "/dev/disk/by-id/nvme-CT4000P3PSSD8_2334E86EFD15";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "2G";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "buni";
+                settings = {
+                  #aaaa
+                  allowDiscards = true;
+                };
+                extraFormatArgs = [
+                  "--iter-time 2137"
+                ];
+                content = {
+                  type = "btrfs";
+                  extraArgs = [
+                    "-L"
+                    "bunie"
+                  ];
+                  subvolumes = {
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "subvol=nix"
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/silly" = {
+                      mountpoint = "/silly";
+                      mountOptions = [
+                        "subvol=silly"
+                        "compress=zstd"
+                        "relatime"
+                      ];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = [
+                        "subvol=home"
+                        "compress=zstd"
+                        "relatime"
+                      ];
+                    };
+                    "/log" = {
+                      mountpoint = "/var/log";
+                      mountOptions = [
+                        "subvol=log"
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/awawa" = {
+                      mountpoint = "/awawa";
+                      swap.swapfile.size = "48G";
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    nodev = {
+      "/" = {
+        fsType = "tmpfs";
+        mountOptions = [
+          "mode=755"
+          "uid=0"
+          "gid=0"
+        ];
+      };
+    };
+  };
+
+  fileSystems."/silly".neededForBoot = true;
+  environment.etc.adjtime.source = lib.mkForce "/silly/etc/adjtime";
+
+#  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
---
+++ host/puppyconsol/noelle.nix
@@ -0,0 +1,130 @@
+{
+  osConfig,
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    ../../home
+    inputs.catppuccin.homeModules.catppuccin
+  ];
+
+  home.packages = with pkgs; [
+    sops
+    age
+    heroic
+    hydralauncher
+    kdePackages.kate
+    telegram-desktop
+    signal-desktop
+#    tor-browser
+    krita
+    spotify #change to idk spotifyd at some point
+    motrix-next
+    ];
+
+  catppuccin = {
+    enable = true;
+    flavor = "macchiato";
+    accent = "mauve";
+  };
+
+  programs.kitty.settings = {
+    font_family = "Comic Code Ligatures";
+    font_features = "+frac +liga +ordn +sinf";
+  };
+
+  bunos.net.syncthing = {
+    enable = true;
+    cert = osConfig.age.secrets.sync.path;
+    key = osConfig.age.secrets.synck.path;
+  };
+
+  bunos.desktop.gaming.minecraft.enable = true;
+
+  programs.senpai.config = {
+    nickname = "noelle";
+    password-cmd = [
+      "cat"
+      "${osConfig.age.secrets.irc.path}"
+    ];
+  };
+
+  services.recoll = {
+    enable = false;
+    # TODO SMTH IDK WRUF :3
+  };
+
+  systemd.user.services = {
+    adjust-audio = {
+      Unit = {
+        Description = "adjust auwudio";
+      };
+
+      Install = {
+        WantedBy = [ "wireplumber.service" ];
+      };
+
+      Service = {
+        Type = "oneshot";
+        ExecStart = "wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 5";
+      };
+    };
+  };
+#   android_sdk.accept_license = true;
+
+/*  programs.thunderbird = {
+    enable = true;
+  }; */
+
+
+/*  programs.firefox = {
+    enable = true;
+    package = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  }; */ # moved to browsers directory
+
+/*  programs.nushell = {
+    enable = true;
+  };
+
+  bunos.net.syncthing = {
+    enable = true;
+#    cert =
+#    key =  # LATER
+  }; */
+
+/*
+
+  programs.obs-studio = {
+    enable = true;
+
+    package = (
+      pkgs.obs-studio.override {
+        cudaSupport = true;
+      }
+    );
+
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
+
+*/
+
+
+
+/*  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 7d --keep 5";
+    flake = "/home/noa/config";
+  }; */
+}
